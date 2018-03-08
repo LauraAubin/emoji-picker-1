446afab84b45bbcc583377ec9c41590104fdada4
Mateus Ferreira Silva <mtsferreirasilva@gmail.com>
2018-03-08 00:29:33 -0500

    Adding Shopify Polaris lib and cleaning up code

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import '@shopify/polaris/styles.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
